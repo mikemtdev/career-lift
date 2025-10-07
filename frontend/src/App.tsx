d304cb2c42785874c1d2e28b05a20f0a6fd30ff7
Mike M.T Njovu <mikemtnjovu@gmail.com>
2025-10-07 16:54:43 +0200

    Import Navigate from react-router-dom for route redirection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Landing } from './pages/Landing';
 import { Login } from './pages/Login';
 import { Signup } from './pages/Signup';
